235041ef53b8554fa7d58937f09944548ddc193b
monaSalih <monaalobisat@gmail.com>
2021-06-20 16:30:41 +0300

    newLabe

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let a1= prompt("what is your name?");
+
+let a2=prompt("what are your favort anime?");
+
+let a3= prompt("what is the last anime you see?");
+
+
+let a4= prompt("did you prefare long series or short?");
+
+
+alert("Hello "+ " "+ a1 +" in our page" +" "+a2 +" good choose"
++" "+a3 +" beautiful one"+ " "+a4 +" ooooh we are in same page now ");
+
+
+console.log("what is your name?"+ a1);
+console.log("what are your favort anime?"+ a2);
+console.log("what is the last anime you see?"+ a3);
+console.log("did you prefare long series or short?"+ a4);
